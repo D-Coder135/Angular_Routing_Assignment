9cb95252360dc768fbd23b830a2c001d6c10ac0c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-28 22:54:57 +0530

    initialize the field user along with it's property id and name inside the ngOnInit() method body

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -12,6 +12,10 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
   }
 
 }
