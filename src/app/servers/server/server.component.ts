26814fddfbea982500166287cde6eacc1bccebe5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-30 13:42:47 +0530

    add a es6 (lambda) function as the parameter inside the subscribe() method call

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -18,7 +18,9 @@ export class ServerComponent implements OnInit {
     const id = this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
-      
+      (params: Params) => {
+        this.server = this.serversService.getServer(params['id']);
+      }
     );
   }
 
