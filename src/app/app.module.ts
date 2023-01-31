fc5179c1708e940dd1bbd23dce7ff326279b9ea4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-01-31 15:34:50 +0530

    update the path of a route with '**' in order to make it a wild-card route for catching all the unmatched routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
     { path: ':id/:name', component: UserComponent},
   ]},
   { path: 'not-found', component: PageNotFoundComponent },
-  { path: 'something', redirectTo: '/not-found' }
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
