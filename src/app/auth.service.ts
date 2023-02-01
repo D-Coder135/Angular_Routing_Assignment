42d09fc66363a6867b0f63159cbc15865d70cb68
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 14:16:04 +0530

    add some code in order to fake the authentication behaviour inside the project

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,13 @@ export class AuthService {
     loggedIn = false;
 
     isAuthenticated() {
-        
+        const promise = new Promise(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn);
+                }, 800)
+            }
+        )
     }
 
     login() {
