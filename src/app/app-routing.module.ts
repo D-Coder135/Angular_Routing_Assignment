ee362ffeda9c9a617a3f1a2827b03372f2138a61
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 13:53:36 +0530

    paste some part of the code inside the app-routing.module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from "@angular/core";
 
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'servers', component: ServersComponent, children: [
+      { path: ':id/edit', component: EditServerComponent},
+      { path: ':id', component: ServerComponent},
+    ]},
+    { path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent},
+    ]},
+    { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }
+  ];
+
 @NgModule({
-    
+
 })
 export class AppRoutingModule {
 
