2762abd655b2a6ed8b0be47d8b817b081146bb2e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 14:29:07 +0530

    return the canActivate() method call

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
@@ -20,6 +20,10 @@ export class AuthGuard implements CanActivate, CanActivateChild {
                     }
                 }
             );
+    }
+
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.canActivate(childRoute, state);
     }
 
 }
