b98676e19adc888a4b8dd519a8e003da1e17681c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 13:54:30 +0530

    add required imports in order to resolve the errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
