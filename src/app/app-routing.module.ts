d886d59178e5aae3d26119d327ff79f4f726d8a1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 13:57:45 +0530

    call the RouterModule.forRoot() method and pass the appRoutes array object to it as a parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -23,6 +23,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(appRoutes)
     
   ],
 })
