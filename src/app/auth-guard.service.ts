ed6ba0b7e30a1c317a90531fb51eef176f9d56f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-01 14:21:12 +0530

    add some return statements

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -10,7 +10,16 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-            
+            return this.authService.isAuthenticated().then(
+                (authenticated: boolean) => {
+                    if(authenticated) {
+                        return true;
+                    } else {
+                        this.router.navigate(['/']);
+                        return false;
+                    }
+                }
+            );
     }
 
 }
