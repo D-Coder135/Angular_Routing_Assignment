4da8fa4c6181b8c2bf83003bc066e745671c553b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-02 23:18:05 +0530

    add an else block for the previously added if() block

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -46,6 +46,8 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
 
     if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
       return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
     }
   }
 
