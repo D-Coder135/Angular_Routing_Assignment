33d96457b75c246e16cf063023df15ad10f014d0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2023-02-02 23:03:46 +0530

    update the value for changesSaved field inside the onUpdateServer() method body

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -33,6 +33,7 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
   }
 
 }
